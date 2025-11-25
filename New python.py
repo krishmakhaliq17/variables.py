822338855a49af0904af3beb6b65e115e4ed2111
KRISH <you@example.com>
2025-11-25 20:32:33 +0100

    Initial commit

---
+++ New python.py
@@ -0,0 +1,36 @@
+# Hardcoded food items
+food1 = "Pizza"
+food2 = "Burger"
+food3 = "Pasta"
+
+# Print them in one line
+print(food1, food2, food3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
---
+++ first_Name = Mario.py
@@ -0,0 +1,14 @@
+Hello = 'Hello!'
+name = 'John Doe'
+age = '40'
+
+print(len(Hello))
+print(len(name))
+print(len(age))
+
+
+
+
+
+
+
